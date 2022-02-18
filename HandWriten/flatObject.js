d3419c2281274860bc370daa0d84f9eb90bef09e
charliemills <charliemillscn@gmail.com>
2022-02-18 16:46:20 +0800

    flatObjects.js

---
+++ HandWriten/flatObject.js
@@ -0,0 +1,17 @@
+function objectFlat(obj = {}) {
+  let res = {};
+  function flat(item, preKey = "") {
+    Object.entries(item).forEach(([key, val]) => {
+      const newKey = preKey ? `${preKey}.${key}` : key;
+      if (val && typeof val === "object") {
+        flat(val, newKey);
+      } else {
+        res[newKey] = val;
+      }
+    });
+  }
+  flat(obj);
+  return res;
+}
+const source = { a: { b: { c: 1, d: 2 }, e: 3 }, f: { g: 2 } };
+console.log(objectFlat(source));
