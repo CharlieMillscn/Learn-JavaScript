03719e3e9f181a8cd0a85aede246a4da71b1a86f
charliemills <charliemillscn@gmail.com>
2022-03-12 22:36:14 +0800

    update

--- 赛码网输入输出.js
+++ 赛码网输入输出.js
@@ -1,23 +1,19 @@
-var coinChange = function (coins, amount) {
-  const n = coins.length;
-  if (n == 0) return -1;
-  let dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (let i = 1; i <= amount; i++) {
-    for (let coin of coins) {
-      if (i - coin >= 0) {
-        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-      }
+const helper = (data) => {
+  const n = data.length;
+  let ans = 0;
+  data.forEach((element) => {
+    if (element === "1") ans++;
+  });
+  for (let i = 0; i < n; i++) {
+    let temp = +data[i];
+    for (let j = i + 1; j < n; j++) {
+      temp *= +data[j];
+      if (temp === 1) ans++;
     }
   }
-  return dp[amount] <= amount ? dp[amount] : "Impossible";
+  return ans;
 };
-let line;
-while ((line = read_line())) {
-  if (parseInt(line) === 0) break;
-  let temp = read_line()
-    .split(" ")
-    .slice(1)
-    .map((item) => parseInt(item, 0));
-  print(coinChange(temp, parseInt(line)));
-}
+console.log(helper("-1 1 1".split(" ")));
+// let number = readInt(),
+//   line = read_line();
+// console.log(helper(line.split(" ")));
